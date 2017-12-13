885b8e3e15edc0e09edaad641d4e6a45da2385c2
OldGreg <flyers08@vt.edu>
2017-12-13 11:48:27 -0500

    Added Nickel server for hosting the applications and implemented threading for the ws server and app router.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 extern crate ws;
+extern crate nickel;
 
 use std::thread;
+use std::time;
+use std::env;
 
 use ws::{listen, Handler, Sender, Result, Message, Handshake, CloseCode, Error};
+use nickel::{Nickel, Mountable, StaticFilesHandler};
 
 struct Server {
     out: Sender,
 }
 
 impl Handler for Server {
-
     fn on_open(&mut self, _: Handshake) -> Result<()> {
         Ok(println!("The client has connected"))
     }
@@ -32,17 +35,33 @@ impl Handler for Server {
     fn on_error(&mut self, err: Error) {
         println!("The server encountered an error: {:?}", err);
     }
-
 }
 
 fn main() {
-  let ws_server = thread::Builder::new().name("ws_server".to_string()).spawn(move || {
+  let mut prod_env = "".to_string();
+
+  let ws_server_thread = thread::Builder::new().name("ws_server".to_string()).spawn(move || {
+      println!("Starting websocket server..");
       listen("127.0.0.1:3012", |out| { Server { out: out } }).unwrap()
   }).unwrap();
-  let app_router = thread::Builder::new().name("app_router".to_string()).spawn(move || {
-      println!("Router")
-  }).unwrap();
-  let _ = ws_server.join();
-  let _ = app_router.join();
+
+  thread::sleep(time::Duration::from_millis(1000));
+  match env::var("PROD_ENV".to_string()) {
+      Ok(val) => prod_env = val,
+      Err(e) => println!("Operating in dev mode due to: {}", e),
+  }
+
+  if prod_env != "" {
+    let app_router_thread = thread::Builder::new().name("app_router".to_string()).spawn(move || {
+        let mut app_router = Nickel::new();
+        println!("Starting app router..");
+        app_router.mount("/controller/", StaticFilesHandler::new("app/controller/"));
+        app_router.mount("/display/", StaticFilesHandler::new("app/display/"));
+        app_router.listen("127.0.0.1:6767").unwrap();
+    }).unwrap();
+    let _ = app_router_thread.join();
+  }
+
+  let _ = ws_server_thread.join();
   println!("Server closing down..");
 }
