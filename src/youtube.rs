8be1734bfdaf24f23339bd8b78c4634f28e19512
Greg Scott <gregscott94@vt.edu>
2018-01-14 18:30:26 -0500

    Starting Youtube search

--- src/youtube.rs
+++ src/youtube.rs
@@ -1,4 +1,12 @@
 
+const API_BASE: &str = "https://www.googleapis.com/youtube/v3";
+
 pub struct Youtube {
     api_key: String,
+}
+
+impl Youtube {
+    fn search(&self, query: &str) {
+        let request_string = format!("{}/search?q={}&maxResults=25&part=snippet&key={}", API_BASE, query, self.api_key);
+    }
 }
